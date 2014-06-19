1213f948ae4fdccc89c72749703fa4624dd44aa9
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 00:44:56 -0400

    Update to v3

--- src/leveldb.h
+++ src/leveldb.h
@@ -1,8 +1,9 @@
 // Copyright (c) 2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_LEVELDB_H
-#define BITCOIN_LEVELDB_H
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+#ifndef MEMORYCOIN_LEVELDB_H
+#define MEMORYCOIN_LEVELDB_H
 
 #include "serialize.h"
 
@@ -150,4 +151,4 @@ public:
     }
 };
 
-#endif // BITCOIN_LEVELDB_H
+#endif // MEMORYCOIN_LEVELDB_H
