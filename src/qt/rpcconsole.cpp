5dc041ff92e156c7069bb6717ff41c182e3ac62b
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 02:52:23 -0400

    Update to v3

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1,8 +1,14 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
 #include "rpcconsole.h"
 #include "ui_rpcconsole.h"
 
 #include "clientmodel.h"
-#include "bitcoinrpc.h"
+#include "nemorycoinrpc.h"
 #include "guiutil.h"
 
 #include <QTime>
