daa5bdfb7d916c279d4557ca47bda2b5b6bec05a
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 00:15:15 -0400

    Update to v3

--- src/serialize.h
+++ src/serialize.h
@@ -2,8 +2,10 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_SERIALIZE_H
-#define BITCOIN_SERIALIZE_H
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
+#ifndef MEMORYCOIN_SERIALIZE_H
+#define MEMORYCOIN_SERIALIZE_H
 
 #include <string>
 #include <vector>
