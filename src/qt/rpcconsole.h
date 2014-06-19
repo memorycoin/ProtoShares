427e751dad8ea746358b289c618d198bd5cf3553
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 02:53:21 -0400

    Update to v3

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
 #ifndef RPCCONSOLE_H
 #define RPCCONSOLE_H
 
@@ -8,7 +14,7 @@ namespace Ui {
 }
 class ClientModel;
 
-/** Local Bitcoin RPC console. */
+/** Local Memorycoin RPC console. */
 class RPCConsole: public QDialog
 {
     Q_OBJECT
@@ -35,7 +41,7 @@ private slots:
     void on_tabWidget_currentChanged(int index);
     /** open the debug.log from the current datadir */
     void on_openDebugLogfileButton_clicked();
-    /** display messagebox with program parameters (same as bitcoin-qt --help) */
+    /** display messagebox with program parameters (same as memorycoin-qt --help) */
     void on_showCLOptionsButton_clicked();
 
 public slots:
