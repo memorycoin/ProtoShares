d50a80dd80e33e23105ad6f9dc36a3f6aa6be99a
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 00:45:18 -0400

    Update to v3
    Added CR

--- src/leveldb.cpp
+++ src/leveldb.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+// Copyright (c) 2013-2014 Memorycoin Dev Team
 #include "leveldb.h"
 #include "util.h"
 
