de63b9398ba2e7e310878381dfa279a8cb9667d5
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 00:12:20 -0400

    Update to v3

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -2,8 +2,10 @@
 // Copyright (c) 2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_UI_INTERFACE_H
-#define BITCOIN_UI_INTERFACE_H
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
+#ifndef MEMORYCOIN_UI_INTERFACE_H
+#define MEMORYCOIN_UI_INTERFACE_H
 
 #include <string>
 #include "util.h" // for int64
