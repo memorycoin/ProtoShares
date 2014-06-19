db114638b109662a171689fac85c3da225254950
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 03:29:36 -0400

    Update to v3

--- src/sync.cpp
+++ src/sync.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2011-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2013-2014 Memorycoin Dev Team
 
 #include "sync.h"
 #include "util.h"
