4b2b13a41e1822f38ee552484edffa7cb8a4c315
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 03:50:55 -0400

    Update to v3

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
 #include "hash.h"
 
 inline uint32_t ROTL32 ( uint32_t x, int8_t r )
