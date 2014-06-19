dc729678871353c186b06dc3ec1d9fdf3e685011
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 01:22:27 -0400

    Update to v3

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -1,4 +1,10 @@
-#define BOOST_TEST_MODULE Bitcoin Test Suite
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+
+#define BOOST_TEST_MODULE Memorycoin Test Suite
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
 
