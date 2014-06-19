d881beba7f539f74965b17650cf355a0e669436b
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 03:54:34 -0400

    Update to v3

--- src/bloom.h
+++ src/bloom.h
@@ -1,8 +1,9 @@
 // Copyright (c) 2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_BLOOM_H
-#define BITCOIN_BLOOM_H
+// Copyright (c) 2013-2014 Memorycoin Dev Team
+#ifndef MEMORYCOIN_BLOOM_H
+#define MEMORYCOIN_BLOOM_H
 
 #include <vector>
 
@@ -88,4 +89,4 @@ public:
     void UpdateEmptyFull();
 };
 
-#endif /* BITCOIN_BLOOM_H */
+#endif /* MEMORYCOIN_BLOOM_H */
