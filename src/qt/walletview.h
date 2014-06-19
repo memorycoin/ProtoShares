5fcce9f2962ffaf19db5b72c4c6a9dc8284266a7
Delinquency <Delinquency@users.noreply.github.com>
2014-06-19 01:25:38 -0400

    Update to v3

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -1,15 +1,16 @@
 /*
- * Qt4 bitcoin GUI.
+ * Qt4 Memorycoin GUI.
  *
  * W.J. van der Laan 2011-2012
  * The Bitcoin Developers 2011-2013
+ * Copyright (c) 2013-2014 Memorycoin Dev Team
  */
 #ifndef WALLETVIEW_H
 #define WALLETVIEW_H
 
 #include <QStackedWidget>
 
-class BitcoinGUI;
+class MemorycoinGUI;
 class ClientModel;
 class WalletModel;
 class TransactionView;
@@ -37,16 +38,16 @@ class WalletView : public QStackedWidget
     Q_OBJECT
 
 public:
-    explicit WalletView(QWidget *parent, BitcoinGUI *_gui);
+    explicit WalletView(QWidget *parent, MemorycoinGUI *_gui);
     ~WalletView();
 
-    void setBitcoinGUI(BitcoinGUI *gui);
+    void setMemorycoinGUI(MemorycoinGUI *gui);
     /** Set the client model.
         The client model represents the part of the core that communicates with the P2P network, and is wallet-agnostic.
     */
     void setClientModel(ClientModel *clientModel);
     /** Set the wallet model.
-        The wallet model represents a bitcoin wallet, and offers access to the list of transactions, address book and sending
+        The wallet model represents a Memorycoin wallet, and offers access to the list of transactions, address book and sending
         functionality.
     */
     void setWalletModel(WalletModel *walletModel);
@@ -56,7 +57,7 @@ public:
     void showOutOfSyncWarning(bool fShow);
 
 private:
-    BitcoinGUI *gui;
+    MemorycoinGUI *gui;
     ClientModel *clientModel;
     WalletModel *walletModel;
 
