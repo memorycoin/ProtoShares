02e6ea8f1dbdc056471bf7e90f32c5096befff5e
Delinquency <appece43@tushmail.com>
2014-11-11 19:17:50 -0500

    Update Versioning

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70101;
+static const int PROTOCOL_VERSION = 70102;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 210;
